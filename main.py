9e732e372ee023fa51c3105154e346b77a30e498
Alain ILUNGA <alingocl@gmail.com>
2023-09-25 19:40:57 -0700

    Before function

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-import home, stock
 
 st.set_page_config(
     page_title="Bailch Smart Visibility"
 )
+
 with st.sidebar:
-    choose = option_menu("Smart Visibility", ["Réquisitions","Stocks","Services","Commandes","Déclarations de change","Contact"],
-                         icons=['person-square','graph-up','tools','cart','share','send'],
-                         #menu_icon="app-indicator", default_index=0,
+    choose = option_menu("Smart Visibility", ["Accueil","Réquisitions","Stocks","Services","Commandes","Déclarations de change","Contact"],
+                         icons=['house','person-square','graph-up','tools','cart','share','send'],
+                         menu_icon="bricks", default_index=0,
                          styles={
         "container": {"padding": "5!important", "background-color": "#ffffe0"},
-        "icon": {"color": "red", "font-size": "20px"}, 
+        "icon": {"color": "red", "font-size": "16px"}, 
         "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#ddd"},
         "nav-link-selected": {"background-color": "#add8ff"},
     }
     )
+
+#if choose == "Accueil":
+#    accueil
+
+#if choose == "Réquisitions":
+#    requisitions
+
+#if choose == "Stocks":
+#    stocks
+
+#if choose == "Services":
+#    services
+
+#if choose == "Commandes":
+#    commandes
+
+#if choose == "Déclarations de change":
+#    declarations
+
+#if choose == "Contact":
+#    contact
