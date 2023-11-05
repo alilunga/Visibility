790ccea7cf4fae5398b55315d188237c20e8febe
Alain ILUNGA <alingocl@gmail.com>
2023-11-04 17:12:25 -0700

    Fix Deta access

--- accueil.py
+++ accueil.py
@@ -10,7 +10,7 @@ def accueilf():
         submitted = st.form_submit_button("Enregistrer")
 
 # comment
-    deta = Deta("DATA_KEY")
+    deta = Deta(st.secrets["data_key"])
     db = deta.Base("visible")
     if submitted:
         db.put({"name":name, "age":age})
