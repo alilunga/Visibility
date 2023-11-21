09d7e02bedc5f13a3c04e950b990b74e9e08d24a
alilunga <143321660+alilunga@users.noreply.github.com>
2023-11-21 20:00:55 +0100

    Update accueil.py

--- accueil.py
+++ accueil.py
@@ -1,18 +1,2 @@
 import streamlit as st  # pip install streamlit
 from deta import Deta
-
-def accueilf():
-    with st.form("form", clear_on_submit=True):
-
-        st.write ("Remplir")
-        name = st.text_input("Ton nom")
-        age = st.number_input("Ton age")
-        submitted = st.form_submit_button("Enregistrer")
-
-# comment
-    deta = Deta(st.secrets["data_key"])
-    db = deta.Base("visible")
-    if submitted:
-        db.put({"name":name, "age":age})
-
-
