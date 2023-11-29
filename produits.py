242c41ec588633067e0246bb87d6d2e1a54d17a0
alilunga <143321660+alilunga@users.noreply.github.com>
2023-11-28 16:11:43 -0800

    Update produits.py

--- produits.py
+++ produits.py
@@ -5,15 +5,15 @@ def produitsf():
     
    with st.form("produits", clear_on_submit=True):
 
-        st.write ("Creer un produit")
-        produit_id = st.number_input("Entrez le numero du produit")
+        st.write ("Créer un produit")
+        produit_id = st.number_input("Entrez le numéro du produit")
         produit_nom = st.text_input("Entrez le nom du produit")
         produit_des = st.text_input("Entrez la description du produit")
-        produit_img = st.text_input("Telecharger l'image du produit")
+        produit_img = st.text_input("Télécharger l'image du produit")
         produit_pa = st.number_input("Entrez le prix d'achat du produit")
         produit_pv = st.number_input("Entrez le prix de vente du produit")
-        produit_stk = st.number_input("Entrez la quantite en stock du produit")
-        produit_dmd = st.number_input("Entrez la quantite en demande du produit")
+        produit_stk = st.number_input("Entrez la quantité en stock du produit")
+        produit_dmd = st.number_input("Entrez la quantité en demande du produit")
         submitted = st.form_submit_button("Enregistrer") 
        
    deta = Deta(st.secrets["data_key"])
