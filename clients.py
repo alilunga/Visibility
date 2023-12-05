0be95a0b0617c7c7e61a4618d7c6fa0c8248bca3
alilunga <143321660+alilunga@users.noreply.github.com>
2023-12-04 19:54:55 -0800

    Update clients.py

--- clients.py
+++ clients.py
@@ -10,7 +10,7 @@ def clientsf():
         client_vil = st.text_input("Entrez la ville du client")
         client_pay = st.text_input("Entrez le pays du client")
         client_pho = st.number_input("Entrez le telephone du client")
-        client_crl = st.number_input("Entrez l'email du client")
+        client_crl = st.text_input("Entrez l'email du client")
         submitted = st.form_submit_button("Enregistrer")
 
       
@@ -18,8 +18,8 @@ def clientsf():
     db = deta.Base("client")
     if submitted:
         db.put({"Numero": client_id, 
-                "Adresse": produit_nom, 
-                "Ville": produit_des, 
-                "Pays": produit_img,
-                "Telephone": produit_pa,
-                "Email": produit_pv})
+                "Adresse": client_adr, 
+                "Ville": client_vil, 
+                "Pays": client_pay,
+                "Telephone": client_pho,
+                "Email": client_crl})
