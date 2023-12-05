6fdfc9725c309e2b281e790ca63fb9a669543c47
alilunga <143321660+alilunga@users.noreply.github.com>
2023-12-04 19:30:12 -0800

    Update clients.py

--- clients.py
+++ clients.py
@@ -24,10 +24,10 @@ def clientsf():
                 "Telephone": produit_pa,
                 "Email": produit_pv,
 
-def clientslstf():
-    deta = Deta(st.secrets["data_key"])
-    db = deta.Base("client")
-    db_content = db.fetch().items
-    st.dataframe(db_content)
+#def clientslstf():
+#    deta = Deta(st.secrets["data_key"])
+#    db = deta.Base("client")
+#    db_content = db.fetch().items
+#    st.dataframe(db_content)
   
 
