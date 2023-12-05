474525db2c8a0b3b6af469f2abb65f03ff86ec5c
alilunga <143321660+alilunga@users.noreply.github.com>
2023-12-04 18:56:48 -0800

    Update clients.py

--- clients.py
+++ clients.py
@@ -1,11 +1,9 @@
 import streamlit as st  # pip install streamlit
 
 def clientsf():
-    # --- INPUT & SAVE VALEURS DOUANE ---
 
     with st.form("clients", clear_on_submit=True):
 
-        st.write ("Ajouter un client")
         client_id = st.number_input("Entrez le numero du client")
         client_adr = st.text_input("Entrez l'addresse du client")
         client_vil = st.text_input("Entrez la ville du client")
@@ -14,5 +12,21 @@ def clientsf():
         client_crl = st.number_input("Entrez l'email du client")
         submitted = st.form_submit_button("Enregistrer")
 
+      
+   deta = Deta(st.secrets["data_key"])
+   db = deta.Base("client")
+   if submitted:
+        db.put({"Numero": client_id, 
+                "Adresse": produit_nom, 
+                "Ville": produit_des, 
+                "Pays": produit_img,
+                "Telephone": produit_pa,
+                "Email": produit_pv,
+
+def clientlstf():
+   deta = Deta(st.secrets["data_key"])
+   db = deta.Base("client")
+   db_content = db.fetch().items
+   st.dataframe(db_content)
   
 
