4afa5836ac25e62aca944d18b67a5bff11027c34
alilunga <143321660+alilunga@users.noreply.github.com>
2023-12-11 18:43:03 -0800

    Update fournisseurs.py

--- fournisseurs.py
+++ fournisseurs.py
@@ -13,5 +13,22 @@ def fournisseursf():
         fournisseur_pho = st.number_input("Entrez le telephone du fournisseur")
         fournisseur_crl = st.number_input("Entrez l'email du fournisseur")
         submitted = st.form_submit_button("Enregistrer")
-  
+
+deta = Deta(st.secrets["data_key"])
+   db = deta.Base("fournisseur")
+   if submitted:
+        db.put({"Numero": fournisseur_id, 
+                "Nom": produit_nom, 
+                "Adresse": fournisseur_adr, 
+                "Ville": fournisseur_vil,
+                "Pays": fournisseur_pay,
+                "Telephone": fournisseur_pho,
+                "Email": fournisseur_crl})
+ 
+def fournisseurlstf():
+   deta = Deta(st.secrets["data_key"])
+   db = deta.Base("produit")
+   db_content = db.fetch().items
+   st.dataframe(db_content)
+
 
